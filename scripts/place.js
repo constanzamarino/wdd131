6ce56a8b06bdcc47e28ea4d6f41cbebccca17814
Constanza Mariño <marinoconstanza03@gmail.com>
2026-01-29 18:35:08 -0300

    Classes added in html + design in css for the mobile view done + time and date in js added ; have to add the media query in place-large.css and more js code for the wind chill

---
+++ scripts/place.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+//Current date and time (footer)
+
+const currentYearElement = document.getElementById("currentYear");
+const lastModifiedElement = document.getElementById("lastModified");
+
+const now = new Date();
+currentYearElement.textContent = now.getFullYear();
+
+lastModifiedElement.textContent = new Date(document.lastModified).toLocaleDateString("en-US");
+
+const currentTime = document.getElementById("currentTime");
+setInterval(function(){
+    const time = new Date();
+    const hours= time.getHours().toString();
+    const minutes= time.getMinutes().toString().padStart(2,'0');
+    const seconds= time.getSeconds().toString().padStart(2, '0');
+
+    currentTime.textContent= ` ${hours}:${minutes}:${seconds}`;
+}, 1000);
