51a456f8e406823949468513236873200672c287
Constanza Mariño <marinoconstanza03@gmail.com>
2026-02-07 11:54:50 -0300

    Starting temples.html enhacement

---
+++ scripts/temples-filtered.js
@@ -0,0 +1,29 @@
+//Footer with the current year + last modified date + current time
+
+const currentYearElement = document.getElementById("currentYear");
+const lastModifiedElement = document.getElementById("lastModified");
+
+const now = new Date();
+currentYearElement.textContent = now.getFullYear();
+
+lastModifiedElement.textContent = new Date(document.lastModified).toLocaleDateString("en-US");
+
+const currentTime = document.getElementById("currentTime");
+setInterval(function(){
+    const time = new Date();
+    const hours= time.getHours().toString();
+    const minutes= time.getMinutes().toString().padStart(2,'0');
+    const seconds= time.getSeconds().toString().padStart(2, '0');
+
+    currentTime.textContent= ` ${hours}:${minutes}:${seconds}`;
+}, 1000);
+
+// Hamburger button for the nav menu (mobile view)
+
+const nav= document.querySelector(".navigation-menu");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener('click', () => {
+    nav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+})
